42efe0729371a2d6ab6fdc93cbcee949d0f3827f
Deepu Pandey <pandeydeepu903@gmail.com>
2026-02-09 22:45:07 +0530

    this is Palindrome Linked List problem

---
+++ LeetCode(JAVA(Q and A))/Palindrome_Linked_List.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head==null||head.next==null){
+            return true;
+        }
+        ListNode middle = findMiddle(head);
+        ListNode secondHalfStart = reverse(middle.next);
+        ListNode firstHalfStart=head;
+        while(secondHalfStart!=null){
+            if(firstHalfStart.val!=secondHalfStart.val){
+                return false;
+            }
+            firstHalfStart=firstHalfStart.next;
+            secondHalfStart=secondHalfStart.next;
+        }
+        return true;
+    }
+    ListNode findMiddle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
--- LeetCode(JAVA(Q and A))/Redime.md
+++ LeetCode(JAVA(Q and A))/Redime.md
@@ -18,6 +18,11 @@ Q4. Slow and Fast Pointers use and Problems:-->
 
     1--> for detection cycle :- 141. Linked List Cycle
     2--> Find middle node :- 876. Middle of the Linked List
+    3-->234. Palindrome Linked List
+      explaination: 3 step of this problem 
+         1.find middle of LL
+         2.second half reverse
+         3.check first half and second half
 
                
                
